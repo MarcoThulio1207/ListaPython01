b1aa78b679491c6e0682b4fb4b5947f612b0149d
Marco Thulio <65259289+MarcoThulio1207@users.noreply.github.com>
2023-06-22 00:13:41 -0300

    Add files via upload

---
+++ src/Ex01.java
@@ -0,0 +1,8 @@
+public class Ex01 {
+    public static void main(String[] args) {
+                //Exercico 1 - feito por Thulio;
+                //Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+                System.out.println("Olá mundo!");
+            }
+        }
+
